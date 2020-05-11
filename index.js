69341ccdbfc26aa73226faef44a60561bf51edab
Christopher D Couture <videogameroulette@yahoo.ca>
2020-05-11 18:15:29 -0400

    Final Commit

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 function loadXMLDoc() {
-  const xhr = new XMLHttpRequest();
-  const xml = "0.xml";
-  xhr.open("GET", xml);
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      getData(this);
-    }
-  };
-  xhr.send();
+  var file = document.querySelector('#inputId').files[0];
+  var fr = new FileReader();
+  fr.onload = e => getData(e.target.result);
+  fr.readAsText(file);
 }
 
 function getData(xml) {
-  var x, i, xmlDoc, txt;
-  xmlDoc = xml.responseXML;
+  const domParser = new DOMParser();
+  const xmlDoc = domParser.parseFromString(xml, 'text/html');
   locationID = xmlDoc.getElementsByTagName("VanillaPlacement");
   locationName = xmlDoc.getElementsByTagName("Name");
   itemName = xmlDoc.getElementsByTagName("Item");
-  for (i = 0; i< 124; i++) {
+  for (var i = 0; i< 124; i++) {
     var itemID = "item" + locationID[i].innerHTML;
     var itemURL = `items/${itemName[i].innerHTML}.svg`;
     var url = `url(${itemURL})`;
@@ -28,12 +23,4 @@ function setData(_itemID, _url, _itemName) {
   if (_itemName == "Lore") { _url = `url(items/DigitalPaper.svg)`; }
   else if (_itemName == "Empty") { _url = ""; }
   document.getElementById(_itemID).style.background = _url;
-  logMe(_itemID, _url, _itemName)
-}
-
-function logMe(_itemID, _url, _itemName) {
-  console.log(`Item ${_itemName} Placed`);
-  console.log(_itemID);
-  console.log(_url);
-  console.log("------------------------------------");
 }
