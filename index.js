13993652283073843034d54a00d958170b3ba99e
Christopher D Couture <videogameroulette@yahoo.ca>
2020-05-11 21:46:36 -0400

    UPDATE 1.1

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 function loadXMLDoc() {
-  var file = document.querySelector('#inputId').files[0];
-  var fr = new FileReader();
-  fr.onload = e => getData(e.target.result);
-  fr.readAsText(file);
+
+  let file = document.querySelector('#inputId').files[0]; // GET FILE FROM USER INPUT
+  let fr = new FileReader(); // CREATE NEW INSTANCE OF FILEREADER CLASS
+  fr.onload = e => getData(e.target.result); // ON FILE LOAD RUN GETDATA FUNCTION
+  fr.readAsText(file); // READ FILE AS TEXT
+
 }
 
 function getData(xml) {
-  const domParser = new DOMParser();
-  const xmlDoc = domParser.parseFromString(xml, 'text/html');
-  locationID = xmlDoc.getElementsByTagName("VanillaPlacement");
-  locationName = xmlDoc.getElementsByTagName("Name");
-  itemName = xmlDoc.getElementsByTagName("Item");
+
+  const domParser = new DOMParser(); // CREATE NEW INSTANCE OF DOMPARSER CLASS
+  const xmlDoc = domParser.parseFromString(xml, 'text/html'); // PARSE HTML FROM FILE TEXT STRING USING DONPARSER CLASS
+  const locationID = xmlDoc.getElementsByTagName("VanillaPlacement"); // GET ALL VANILLAPLACEMENT HTML TAGS
+  const itemName = xmlDoc.getElementsByTagName("Item"); // GET ALL ITEM HTML TAGS
+
   for (var i = 0; i< 124; i++) {
-    var itemID = "item" + locationID[i].innerHTML;
-    var itemURL = `items/${itemName[i].innerHTML}.svg`;
-    var url = `url(${itemURL})`;
-    setData(itemID, url, itemName[i].innerHTML);
+    let currentItemID = "item" + locationID[i].innerHTML; // GET AND SET CURRENTITEMID
+    let currentItemName = itemName[i].innerHTML; // GET AND SET CURRENTITEMNAME
+    let currentItemPath = `items/${currentItemName}.svg`; // GET AND SET CURRENTITEMPATH
+    let currentURL = `url(${currentItemPath})`; // GET AND SET CURRENTURL
+    setData(currentItemID, currentURL, currentItemName); // RUN SETDATA FUNCTION
   }
+
 }
 
 function setData(_itemID, _url, _itemName) {
-  if (_itemName == "Lore") { _url = `url(items/DigitalPaper.svg)`; }
-  else if (_itemName == "Empty") { _url = ""; }
-  document.getElementById(_itemID).style.background = _url;
+
+  if (_itemName == "Lore") { _url = `url(items/DigitalPaper.svg)`; } // IF ITEM IS LORE REPLACE URL WITH CORRECT URL PATH
+  else if (_itemName == "Empty") { _url = ""; } // IF ITEM IS EMPTY SET ITEM URL PATH TO EMPTY STRING
+  document.getElementById(_itemID).style.background = _url; // SET DOM ITEM BACKGROUND 
+
 }
